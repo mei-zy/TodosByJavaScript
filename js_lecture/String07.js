386afebb786eded7b7d72cfe197416e5fec107a2
urther <alswlkku@ajou.ac.kr>
2021-12-12 19:21:51 +0900

    js study - string method

--- js_lecture/String07.js
+++ js_lecture/String07.js
@@ -1,5 +1,17 @@
-const compress = (str) =>{
-  let set=new Set();
-  str.replace(/[a-zA-Z]/g, (count) => );
+const compress = word => {
+  const alphabet = Array.from(new Array(26), (x, i) => String.fromCharCode(65 + i));
+
+  let answer = '';
+  const checked_dup = [];
+  [...word].map((x, i) => {
+    const reg = RegExp(`${x}{2,}`, 'g');
+
+    if (checked_dup[checked_dup.length - 1] >= i) return;
+
+    reg.test(word) ? checked_dup.push(i + word.match(reg)[0].length - 1) : false;
+    reg.test(word) ? (answer += x + word.match(reg)[word.match(reg).length - 1].length + '') : (answer += x);
+    return;
+  });
+  return answer;
 };
-console.log(compress("ABBCCCE"));
+console.log(compress('ABBBEEEFFFBB'));
