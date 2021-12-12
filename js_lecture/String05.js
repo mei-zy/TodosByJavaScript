51483250fd6ff7d83f55a50d4becc9e0811d203a
urther <alswlkku@ajou.ac.kr>
2021-12-12 11:19:06 +0900

    js study - string method

--- js_lecture/String05.js
+++ js_lecture/String05.js
@@ -1,27 +1,22 @@
-function solution(new_id) {
-  var answer = "";
+function Recommend_id(new_id) {
+  var answer = '';
 
-  // 1. 소문자 => 대문자로 변경
-  if (/[A-Z]/g.test(new_id)) new_id = new_id.toLowerCase();
-  // 2. 특수 문제 빼기
-  new_id = new_id.replace(/[^a-z0-9-_.]/g, "");
-  // 3. 두번 이상 마침표 => . 으로 대체
-  new_id = new_id.replace(/\.{2,}/g, ".");
+  answer = new_id
+    .replace(/[A-Z]/g, upper => upper.toLowerCase())
+    .replace(/[^\w-.]/g, '')
+    .replace(/\.{2,}/g, '.')
+    .replace(/^\.|\.$/g, '')
+    .replace(/^$/, 'a')
+    .slice(0, 15)
+    .replace(/\.$/g, '');
 
-  // 4. 마침표가 처음이나 끝에 위치한다면 제거
-  new_id = new_id.replace(/^\.|| \.$/g, "");
+  answer.length <= 2 ? (answer += answer[answer.length - 1].repeat(3 - answer.length)) : false;
 
-  // 5. 빈 문자열이면 a를 대입
-  new_id = new_id.replace(" ", "a");
-
-  // 6. 16자리면 15자리만
-  if (new_id.length >= 16) new_id = new_id.substring(0, 15);
-  if (/\.$/.test(new_id)) new_id = new_id.substring(0, new_id.length - 1);
-
-  if (new_id.length <= 2) {
-    if (new_id.length === 0) new_id = "aaa";
-    else new_id += new_id[new_id.length - 1].repeat(3 - new_id.length);
-  }
-  answer = new_id;
   return answer;
 }
+
+console.log(Recommend_id('...!@BaT#*..y.abcdefghijklm.'));
+console.log(Recommend_id(' '));
+console.log(Recommend_id('123_.def'));
+console.log(Recommend_id('z-+.^.'));
+console.log(Recommend_id('abcdefghijklmn.p'));
