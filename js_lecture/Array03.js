b6421e26feed0403b9696334ecc10040d8541fd5
urther <alswlkku@ajou.ac.kr>
2021-12-13 15:23:46 +0900

    Merge branch 'main' of https://github.com/urther/JS_Study

--- js_lecture/Array03.js
+++ js_lecture/Array03.js
@@ -1,14 +1,13 @@
 let todos = [
-  { id: 3, content: "HTML", completed: false },
-  { id: 2, content: "CSS", completed: true },
-  { id: 1, content: "Javascript", completed: false },
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false },
 ];
 
-const sortBy = (todos, key) => {
-  return todos.sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
-};
+const sortBy = (todos, key) =>
+  [...todos].sort((todo1, todo2) => (todo1[key] > todo2[key] ? 1 : todo1[key] > todo2[key] ? -1 : 0));
 
-todos = sortBy(todos, "id");
+todos = sortBy(todos, 'id');
 console.log(todos);
 /*
 [
@@ -17,7 +16,7 @@ console.log(todos);
   { id: 3, content: 'HTML', completed: false }
 ]
 */
-console.log(sortBy(todos, "content"));
+console.log(sortBy(todos, 'content'));
 /*
 [
   { id: 2, content: 'CSS', completed: true },
@@ -25,7 +24,7 @@ console.log(sortBy(todos, "content"));
   { id: 1, content: 'Javascript', completed: false }
 ]
 */
-console.log(sortBy(todos, "completed"));
+console.log(sortBy(todos, 'completed'));
 /*
 [
   { id: 1, content: 'Javascript', completed: false },
