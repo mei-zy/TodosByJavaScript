22c92f889bbb99101671af593034cbd096bf8a48
urther <alswlkku@ajou.ac.kr>
2022-01-02 22:39:09 +0900

    feat : add todos version by Promise
    - todos web application by Promise

---
+++ todos(version-Promise)/public/js/promise.js
@@ -0,0 +1,31 @@
+const req = (method, url, payload) =>
+  new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.send(JSON.stringify(payload));
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        console.log(JSON.parse(xhr.response));
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(new Error(xhr.status));
+      }
+    };
+  });
+
+export default {
+  get(url) {
+    return req("GET", url);
+  },
+  post(url, payload) {
+    return req("POST", url, payload);
+  },
+  patch(url, payload) {
+    return req("PATCH", url, payload);
+  },
+  delete(url) {
+    return req("DELETE", url);
+  },
+};
